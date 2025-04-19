f0b5bac5b9172574d4c876b4532b25796dc8a8a0
Minjae Lee <leem332501@gmail.com>
2025-04-19 17:19:04 +0900

    Initial commit

---
+++ Assets/MainScreen/Miner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Miner : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("게임 시작");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
